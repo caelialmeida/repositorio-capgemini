8d0b4e507b6f3dce9db56efecba23d85a4831fad
caelialmeida <caelialmeida@gmail.com>
2022-02-18 09:06:02 -0300

    Renomear arquivo (retirar o hifen)

---
+++ Questao01.py
@@ -0,0 +1,32 @@
+"""
+Escreva um algoritmo que mostre na tela uma escada de tamanho n utilizando o caractere * e espaços. 
+A base e altura da escada devem ser iguais ao valor de n. 
+A última linha não deve conter nenhum espaço.
+
+Passo 01 - Entrar com número
+Passo 02 - Loop para criar as linhas
+Passo 03 - Calcular os números de espaços vazios e de estrelas a cada linha
+Passo 04 - Usar a função print para "quebrar" a linha
+"""
+
+numero = int(input('Digite um numero:'))
+print(numero)
+espaco = 2 * numero - 2 #espaços vazios entre cada asteriscos
+#Para cada linha há um número diferente de espaços vazios. 
+#Esse é o loop que contabiliza esses espaços vazios.
+
+
+
+for i in range(0, numero):
+
+    #Conta para saber o número de asteriscos * que vão aparecer a cada linha
+    for j in range(0, espaco):
+        print(end=" ") #indica uma nova linha
+    espaco = espaco - 2
+
+    #Loop para imprimir a quantidade de asteriscos a cada linha
+    for j in range(0, i + 1):  
+        print("* ", end="") #Você pode alterar o símbolo impresso, basta trocar o sinal "*"
+
+    print("")
+
---
+++ Questao03.py
@@ -0,0 +1,37 @@
+"""
+Duas palavras podem ser consideradas anagramas de si mesmas se as letras de uma palavra podem ser realocadas para formar a outra palavra. 
+Dada uma string qualquer, desenvolva um algoritmo que encontre o número de pares de substrings que são anagramas.
+
+- Passo 01 - input palavra (variável)
+- Passo 02 - separar substrings
+	#Com um loop para ir do início até o fim da string, e ir adicionando uma letra a cada loop.
+	#Guardar as substrings em um dicionário para poder consultar no próximo passo e evitar repetições ou muita demora.
+- Passo 03 - comparar substrings 
+    #Poderia organizar as letras das palavras em ordem alfabética para facilitar a verificação de anagramas
+- Passo 04 - guardar anagramas (adicionando um contador)
+- Passo 05 - contar a quantidade (variável)
+- Passo 06 - entregar a quantidade
+
+Os anagramas nas substrings não precisam ser com substrings até o final da strings, mas precisa encontrar letras repetidas na string.
+Ex. ifa - fai (a letra "i" se repete após o "a") O contador vai até a próxima letra repetida, não significa que vai dividir a palavra em substrings de tamanhos iguais.
+"""
+palavra = str(input('Sua palavra:'))
+def ContagemDeAnagramas(string):
+    n = len(string)
+    map = dict()
+
+    for i in range(n): #loop pra contar o tamanho das substrings
+        substring = ''
+        for j in range(i, n):
+            substring = ''.join(sorted(substring + palavra[j]))
+            map[substring] = map.get(substring, 0)
+
+            map[substring] += 1 #contagem de substrings 
+
+    anagramas = 0
+
+    for k, v in map.items(): #loop pelo dicionário de substrings 
+        anagramas += (v*(v-1))//2 #fórmula de combinações possíveis
+    return anagramas
+
+print(ContagemDeAnagramas(palavra))
